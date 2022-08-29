2383dd61e1ee46a13b25616f6cd599abc829b4c4
Caio Collet <caio.collet@alunos.sc.senac.br>
2022-08-29 20:24:50 -0300

    Criação da classe pessoa ,carro e barco

---
+++ src/poo/Barco.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo;
+
+/**
+ *
+ * @author caio.collet
+ */
+public class Barco {
+
+    String tipo;
+    String cor;
+    String nome;
+    int numMotores;
+
+    public String getTipo() {
+        return tipo;
+
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+
+    }
+
+    public String getCor() {
+        return cor;
+
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+
+    }
+
+    public String getNome() {
+        return nome;
+
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumMotores() {
+        return numMotores;
+
+    }
+
+    public void setNumMotes(String nome) {
+        this.numMotores = numMotores;
+
+    }
+
+}
---
+++ src/poo/Pessoa.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo;
+
+/**
+ *
+ * @author caio.collet
+ */
+public class Pessoa {
+
+    String nome;
+    String corDoCabelo;
+    String bioTipo;
+    int idade;
+
+    public String getNome() {
+        return nome;
+
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+
+    }
+
+    public String getCorDoCabelo() {
+        return corDoCabelo;
+
+    }
+
+    public void setCorDoCabelo(String CorDoCabelo) {
+        this.corDoCabelo = corDoCabelo;
+
+    }
+
+    public String getBioTipo() {
+        return bioTipo;
+
+    }
+
+    public void setCor(String cor) {
+        this.bioTipo = bioTipo;
+
+    }
+
+    public int getIdade() {
+        return idade;
+
+    }
+
+    public void setCor(int idade) {
+        this.idade = idade;
+
+    }
+
+}
